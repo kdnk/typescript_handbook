dab123c91946527bbb77526ae70591592d33cd16
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2018-02-25 02:08:37 +0900

    Add static side interfacd implementation

--- 02_interfaces/class_types.ts
+++ 02_interfaces/class_types.ts
@@ -12,3 +12,12 @@ class Clock implements ClockInterface {
 
   constructor(h: number, m: number) {}
 }
+
+interface ClockConstructor {
+  new (hour: number, minute: number);
+}
+
+class Clock2 implements ClockConstructor {
+  currentTime: Date;
+  constructor(h: number, m: number) {}
+}
