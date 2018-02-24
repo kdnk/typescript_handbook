3a8d1aaa4144b7e0f706cac578eb13dbb8a51c3f
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2018-02-25 01:32:00 +0900

    string & numeric indexer features

--- 02_interfaces/indexed_types.ts
+++ 02_interfaces/indexed_types.ts
@@ -5,3 +5,21 @@ interface StringArray {
 let myArray: StringArray;
 myArray = ["Bob", "Fred"];
 let myStr: string = myArray[0];
+
+class Animal {
+  name: string;
+}
+
+class Dog extends Animal {
+  breed: string;
+}
+
+// これはダメ
+// numeric indexerの返り値は、string indexerの返り値の
+// subtype出ないといけない。
+// これは、jsは、indexする前に、number --> stringへの
+// 変換を行うため。
+interface NotOkay {
+  [x: number]: Animal;
+  [x: string]: Dog;
+}
