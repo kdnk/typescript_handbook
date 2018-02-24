20f8c8e7621e85232c6412481c4a1d3162e95004
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2018-02-25 00:07:22 +0900

    Can have any type of prop

--- 02_interfaces/exess_prop_check.ts
+++ 02_interfaces/exess_prop_check.ts
@@ -1,6 +1,7 @@
 interface SquareConfig {
   color?: string;
   width?: number;
+  [propName: string]: any;
 }
 
 function createSquare(config: SquareConfig): { color: string; area: number } {
@@ -17,4 +18,4 @@ function createSquare(config: SquareConfig): { color: string; area: number } {
 let mySquare = createSquare({ color: "black", width: 100 });
 console.log(mySquare);
 
-let mySquare2 = createSquare({ width: 100, opacity: 0.5 } as SquareConfig);
+let mySquare2 = createSquare({ width: 100, opacity: 0.5 });
