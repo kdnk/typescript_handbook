3f62f2a49ab06b0f72f962519ff769cc15bc18c6
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2018-02-26 00:03:35 +0900

    constructor_functioins

--- 04_classes/constructor_functions.ts
+++ 04_classes/constructor_functions.ts
@@ -1,13 +1,20 @@
-class Greeter {
+class Greeter1 {
+  static standardGreeting = "Hello, there";
   greeting: string;
   constructor(message: string) {
     this.greeting = message;
   }
   greet() {
-    return "Hello, " + this.greeting;
+    if (this.greeting) {
+      return "Hello, " + this.greeting;
+    } else {
+      return Greeter1.standardGreeting;
+    }
   }
 }
 
-let greeter: Greeter;
-greeter = new Greeter("world");
-console.log(greeter.greet());
+let greeter1: Greeter1;
+greeter1 = new Greeter1("world");
+console.log(greeter1.greet());
+
+let greeterMaker: typeof Greeter1 = Greeter1;
