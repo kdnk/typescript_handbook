2b460afdb16c371938333f7250f4a6dcd67665f8
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2018-02-25 20:38:57 +0900

    Inheritance

--- 04_classes/inheritance.ts
+++ 04_classes/inheritance.ts
@@ -1,16 +1,37 @@
 class Animal {
+  name: string;
+  constructor(theName: string) {
+    this.name = theName;
+  }
   move(distanceInMeters: number = 0) {
-    console.log(`Animal moved ${distanceInMeters}m.`);
+    console.log(`${this.name} moved ${distanceInMeters}m.`);
   }
 }
 
-class Dog extends Animal {
-  bark() {
-    console.log("Woof!");
+class Snake extends Animal {
+  constructor(name: string) {
+    super(name);
+  }
+
+  move(distanceInMeters = 5) {
+    console.log("Slithering...");
+    super.move(distanceInMeters);
   }
 }
 
-const dog = new Dog();
-dog.bark();
-dog.move(10);
-dog.bark();
+class Horse extends Animal {
+  constructor(name: string) {
+    super(name);
+  }
+
+  move(distanceInMeters = 45) {
+    console.log("Galloping...");
+    super.move(distanceInMeters);
+  }
+}
+
+let sam = new Snake("Sammy the Python");
+let tom: Animal = new Horse("Tommy the  Palomino");
+
+sam.move();
+tom.move(34);
