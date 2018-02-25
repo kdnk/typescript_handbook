c4ab6d4b6a3b1452da4aa73dfc35f61a8fea4a1c
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2018-02-25 21:05:25 +0900

    private

--- 04_classes/public.ts
+++ 04_classes/public.ts
@@ -19,3 +19,23 @@ class Animal3 {
 }
 
 new Animal3("Cat").name;
+
+class Animal4 {
+  private name: string;
+  constructor(theName: string) {
+    this.name = theName;
+  }
+}
+
+class Rhino extends Animal4 {
+  constructor() {
+    super("Rhino");
+  }
+}
+
+class Employee {
+  private name: string;
+  constructor(theName: string) {
+    this.name = theName;
+  }
+}
