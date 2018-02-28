057a9e9afbcda7080a0b2f3421e642abe167c75d
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2018-02-28 11:51:52 +0900

    enum as type

---
+++ 07_enums/enums.ts
@@ -0,0 +1,19 @@
+enum ShapeKind {
+  Circle,
+  Square
+}
+
+interface Circle {
+  kind: ShapeKind.Circle;
+  radius: number;
+}
+
+interface Square {
+  kind: ShapeKind.Square;
+  sideLength: number;
+}
+
+let c: Circle = {
+  kind: ShapeKind.Circle,
+  radius: 100
+};
